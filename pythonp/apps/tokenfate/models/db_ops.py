30577a74379601d115bc68b00d2352c1e0682eec
paxzhu <paxzhu98@163.com>
2024-11-19 17:50:04 +0800

    update

--- pythonp/apps/tokenfate/models/db_ops.py
+++ pythonp/apps/tokenfate/models/db_ops.py
@@ -6,7 +6,9 @@ from typing import List, Dict, Any
 from pythonp.apps.tokenfate.models.weight_random import WeightRandom
 import os
 
-data_source = 'pythonp/apps/tokenfate/static/assets/refined_lots.json'
+from pythonp.apps.tokenfate.static.static import get_assets_path
+
+data_source = get_assets_path('refined_lots.json')
 
 class FortunesDatabase:
     def __init__(self, db_name: str = 'fortunes.db'):
