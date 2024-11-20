f648cbebcd0e079284b4d85739a98093eb4216cd
zhengdechang <2473023641@qq.com>
2024-11-20 10:24:00 +0800

    update gunicorn config

--- pythonp/apps/tokenfate/wsgi.py
+++ pythonp/apps/tokenfate/wsgi.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from gunicorn.app.base import BaseApplication
 from uvicorn.workers import UvicornWorker
+import multiprocessing
+import logging
 
 # 加载环境变量
 load_dotenv()
@@ -23,19 +25,25 @@ class StandaloneApplication(BaseApplication):
 
 def run_gunicorn():
     # 从环境变量获取配置
-    workers = os.getenv('WORKERS', '4')
+    workers_env = os.getenv('WORKERS')
     host = os.getenv('HOST', '0.0.0.0')
     port = os.getenv('PORT', '5000')
 
+    if workers_env:
+        workers = int(workers_env)
+    else:
+        workers = multiprocessing.cpu_count() * 2 + 1
+  
     options = {
         'bind': f'{host}:{port}',
-        'workers': int(workers),
+        'workers': int(workers * 2 + 1),
         'worker_class': UvicornWorker,
         'lifespan': 'off',  # 禁用 lifespan
     }
 
     # 假设你的 Flask 应用在 `wsgi.py` 中定义为 `app`
     from app import app
+    logging.info(f"Working config: using {workers} workers")
     StandaloneApplication(app, options).run()
 
 if __name__ == "__main__":
