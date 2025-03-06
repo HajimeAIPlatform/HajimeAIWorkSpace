899d58cec673ddbe1b225ab8c8a3240d7eeea994
zhengdechang <2473023641@qq.com>
2025-03-06 12:31:39 +0800

    add bot_fortune_teller

--- pythonp/apps/fortune_teller/service/__init__.py
+++ pythonp/apps/fortune_teller/service/__init__.py
@@ -12,7 +12,7 @@ from pythonp.apps.fortune_teller.service.dify.views import dify
 from pythonp.apps.fortune_teller.service.fortune_teller_bot.views import run_fortune_teller_bot
 import asyncio
 
-blueprint = Blueprint('api', __name__, url_prefix='/telebot')
+blueprint = Blueprint('api', __name__, url_prefix='/fortune_teller')
 blueprint.register_blueprint(fortune_teller_bot, url_prefix='/bot_fortune_teller')
 blueprint.register_blueprint(ton, url_prefix='/ton')
 blueprint.register_blueprint(dify, url_prefix='/dify')
--- pythonp/apps/fortune_teller/utils/set_webhook.sh
+++ pythonp/apps/fortune_teller/utils/set_webhook.sh
@@ -13,22 +13,12 @@ fi
 # 设置 URL 参数
 WEBHOOK_URL=$1
 BOT_TOKEN="7578797243:AAG4gsM1m4Gn-fj9cZJ98yvbjyO4siSkzC4"
-#BOT_TOKEN="7317644050:AAESKpy0z3-bMZrphdKRKJK925HaquaWQk4"
-
-# BOT_TOKEN="7805651398:AAHCG40KeHJrqRqxXmlA_xDsEWwmMwOpnug"   # fortune_tellerFortuneCookieBot
 
 # 使用 curl 命令设置 Telegram bot 的 webhook
 curl -F "url=$WEBHOOK_URL" https://api.telegram.org/bot$BOT_TOKEN/setWebhook
 
-# bash utils/set_webhook.sh  https://8898-14-155-37-248.ngrok-free.app/telebot/bot/webhook
-# Dev服务器:
-# bash utils/set_webhook.sh https://hajimedev.pointer.ai/telebot/bot/webhook
-# Prod服务器：
-# bash utils/set_webhook.sh https://fortune_teller.pointer.ai/telebot/bot_fortune_teller/webhook
-
-# bash utils/set_webhook.sh https://fortune_teller.pointer.ai/telebot/bot2/webhook
 
 # fortune teller
 # BOT_TOKEN="7578797243:AAG4gsM1m4Gn-fj9cZJ98yvbjyO4siSkzC4"
-# bash utils/set_webhook.sh https://d9ef-14-153-94-63.ngrok-free.app/telebot/bot_fortune_teller/webhook
-# bash utils/set_webhook.sh https://fortune_teller.pointer.ai/telebot/bot_fortune_teller/webhook
+# bash utils/set_webhook.sh https://d9ef-14-153-94-63.ngrok-free.app/fortune_teller/bot_fortune_teller/webhook
+# bash utils/set_webhook.sh https://tokenfate.pointer.ai/fortune_teller/bot_fortune_teller/webhook
